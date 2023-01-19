5fa72a3baeee72b88f5762c13a246ff53c02c9d7
luccaslombardi <luccas.lombardi@hotmail.com>
2023-01-19 20:20:20 -0300

    feat: routes get and post created

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,13 @@
 import fastity from "fastify";
 import cors from "@fastify/cors"
-import { PrismaClient } from "@prisma/client"
+import { appRoutes } from "./routes";
+
 
 const app = fastity();
-const prisma = new PrismaClient();
 
 app.register(cors)
+app.register(appRoutes)
 
-app.get('/', async ()  => {
-    const habits = await prisma.habit.findMany()
-    
-    return habits
-})
 
 app.listen({
     port: 3333
