06217f8eb10e7b53094d6b3c6caf15d056e3a291
luccaslombardi <luccas.lombardi@hotmail.com>
2023-01-22 12:37:51 -0300

    feat: mobile React Navigation config

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 
-export function Habit() {
+export function New() {
     return (
         <View className="flex-1 bg-background px-8 pt-16">
             <Text>
