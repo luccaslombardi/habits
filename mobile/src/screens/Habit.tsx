81d0d0f758efca8c042c7f2303c54c701852a708
luccaslombardi <luccas.lombardi@hotmail.com>
2023-01-22 12:14:10 -0300

    feat: web front style completed

---
+++ mobile/src/screens/Habit.tsx
@@ -0,0 +1,9 @@
+import { Text } from "react-native";
+
+export function Habit() {
+    return (
+        <Text>
+            Habit
+        </Text>
+    )
+}
---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,22 @@
+interface ProgressbarProps {
+    progress: number
+}
+
+export function ProgressBar(props: ProgressbarProps) {
+
+
+    return (
+        <div className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
+            <div
+                role="progressbar"
+                aria-label='progresso de hábitos competados nesse dia'
+                aria-valuenow={props.progress}
+                className='h-3 rounded-xl bg-violet-600 w-3/4'
+                style={{ width: `${props.progress}%` }}
+            />
+
+
+        </div>
+
+    )
+}
